ba30088134452effe386368355292ed0e1db336c
sanmati <sanmatimorbad@gmail.com>
2019-01-11 10:38:14 +0530

    Program to concatenate two input strings

---
+++ concatenate.py
@@ -0,0 +1,6 @@
+str1=input("Enter first string\n")
+"""Enter the first string to be concatenated"""
+str2=input("Enter second string\n")
+"""Enter second string to be concatenated"""
+print(str1+" "+str2)
+"""Prints the concatenated string of str1 and str2"""
