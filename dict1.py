d06c4b28310057c95941c660587ee96c0e6f00e8
sanmati <sanmatimorbad@gmail.com>
2019-01-11 14:31:24 +0530

    Program to to search values in dictionary

--- dict1.py
+++ dict1.py
@@ -1,6 +1,9 @@
 dict1={1:"Speckbit", 2:"World", 3:"Quiet"}
+"""A dictionary in which we want to search a value"""
+
 search=input("Enter the word to be searched")
-for key in dict1:
+"""Value which needs to be searched in dictionary"""
+for key in dict1:  #Traverse through the dictionary
 	if dict1[key]== search:
 		print("True")
 		break
