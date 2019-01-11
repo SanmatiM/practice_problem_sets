98237f52fb50caa94a05ceeb0e743bce263a4aea
sanmati <sanmatimorbad@gmail.com>
2019-01-11 10:48:17 +0530

    Program to print the input statement in multiple lines

---
+++ multiplelines.py
@@ -0,0 +1,4 @@
+str='Hello! My name is Kaustubh. What is your name?'
+"""Statement which should be printed in multiple lines"""
+print ('Hello!\nMy name is Kaustubh.\nWhat is your name?')
+"""Output statements printed in multiple lines"""
