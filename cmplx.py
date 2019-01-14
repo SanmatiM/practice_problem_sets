4a6b25bd91ef81e23c90f2053b40e85e2a2fe476
sanmati <sanmatimorbad@gmail.com>
2019-01-14 10:56:05 +0530

    Program to add two same data types

---
+++ cmplx.py
@@ -0,0 +1,14 @@
+
+m=input("Enter the first input")
+n=input("Enter the second input")
+try:
+	a=float(m)
+	b=float(n)
+	print(y)
+except:
+	y=a+b
+	if (y.isalpha==True):
+		print(y)
+	else:
+		print("Error")
+
